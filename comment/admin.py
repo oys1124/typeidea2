f3a344ef38cf2cb8a75bc3db62c0d023328d26d3
oys1124 <1144166030@qq.com>
2020-06-06 10:45:43 +0800

    diyici gaidong

---
+++ comment/admin.py
@@ -0,0 +1,12 @@
+from django.contrib import admin
+
+from .models import Comment
+
+from typeidea.base_admin import BaseOwnerAdmin
+from typeidea.custom_site import custom_site
+
+# Register your models here.
+
+@admin.register(Comment,site=custom_site)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('target','nickname','content','website','created_time')
